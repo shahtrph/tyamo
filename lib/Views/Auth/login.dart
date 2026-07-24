4f0794b51c1e4d48b1529d404aec3b4afcf17505
megasar <shahpharm517@gmail.com>
2026-07-24 22:36:19 +0500

    26 login screen finished

--- lib/Views/Auth/login.dart
+++ lib/Views/Auth/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
 class Login extends StatelessWidget {
   const Login({super.key});
 
@@ -36,38 +37,99 @@ class Login extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(
-                      width: 05.0,
-                    ),
+                    SizedBox(width: 05.0),
                     Container(
                       height: 30.0,
                       width: 30.0,
-                      child: Image.asset('assets/images/icon1.png',
-                      filterQuality: FilterQuality.high,),
-                    ),
-                  ],
-
-                ),
-                Text(
-                      'to connect with your partner',
-                      style: TextStyle(
-                        color: Colors.indigo,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.bold,
+                      child: Image.asset(
+                        'assets/images/icon1.png',
+                        filterQuality: FilterQuality.high,
                       ),
                     ),
+                  ],
+                ),
+                Text(
+                  'to connect with your partner',
+                  style: TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ],
             ),
-            SizedBox(
-              height: 30,
-            ),
+            SizedBox(height: 30),
             TextField(
               textAlign: TextAlign.start,
               textInputAction: TextInputAction.none,
               obscureText: false,
               autofocus: false,
               keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                label: Text('Email', style: TextStyle(color: Colors.indigo)),
+                fillColor: const Color.fromARGB(255, 247, 238, 238),
+                filled: true,
+                prefixIcon: Icon(
+                  Icons.alternate_email,
+                  size: 15,
+                  color: Colors.indigo,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
+
+            TextField(
+              textAlign: TextAlign.start,
+              textInputAction: TextInputAction.none,
+              obscureText: true,
+              autofocus: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                label: Text('Password', style: TextStyle(color: Colors.indigo)),
+                fillColor: const Color.fromARGB(255, 247, 238, 238),
+                filled: true,
+                prefixIcon: Icon(
+                  Icons.password,
+                  size: 15,
+                  color: Colors.indigo,
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.indigo),
+
+                foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                
+                
+
+              ),
+              onPressed: () {
+                print('Elevated Button Clicked!');
+              },
+              child: const Text('Login'),
+            ),
+
+            SizedBox(height: 30),
+            Text('Forgot Password',
+            style: TextStyle(color: Colors.red,fontSize: 18.0),),
+            Row(
+              
+              children: [
+                
+                Text('Do not have Account ?'),
+                Text('Sign Up')
+              ],
+            )
           ],
         ),
       ),
